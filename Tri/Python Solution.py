8a542d8ca4727067edfdf27b4f0c5c659770869a
ryan mor <56488003+RnMor777@users.noreply.github.com>
2019-11-06 11:21:35 -0600

    Added

---
+++ Tri/Python Solution.py
@@ -0,0 +1,14 @@
+numbs = list(map(int,input().split(' ')))
+ops = ["+","-","/","*"]
+
+for i in range(4):
+    evalString = "{}{}{}".format(numbs[0],ops[i],numbs[1])
+    evalString2 = "{}{}{}".format(numbs[1],ops[i],numbs[2])
+    
+    if eval(evalString+"=={}".format(numbs[2])):
+        print(evalString+"={}".format(numbs[2]))
+        break
+    elif eval("{}==".format(numbs[0])+evalString2):
+        print("{}=".format(numbs[0])+evalString2)
+        break
+
