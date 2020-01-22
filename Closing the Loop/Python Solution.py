16e15746e81d7219bd57be76fc63ee69bf8042d6
Ryan <ryan.morganti@trojans.dsu.edu>
2020-01-21 20:57:02 -0600

    Added comments to the code

--- Closing the Loop/Python Solution.py
+++ Closing the Loop/Python Solution.py
@@ -3,12 +3,16 @@ for i in range(1,int(input())+1):
     ropes = list(input().split(' '))
     length = 0
 
+    #takes the ropes input and splits the lengths of each into either
+    #the b array or the r array, strips it of the letter
     b = [int(x[:-1]) for x in ropes if "B" in x]
     r = [int(x[:-1]) for x in ropes if "R" in x]
 
+    #sorts the arrays
     b.sort(reverse=True)
     r.sort(reverse=True)
 
+    #goes through, to the max of the shortest array to find the length
     for j in range(min(len(b),len(r))):
         length += b[j] + r[j] - 2
 
