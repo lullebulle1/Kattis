c11124f38cf236d6007b8a09496e5e76afa63703
ryan mor <56488003+RnMor777@users.noreply.github.com>
2020-02-12 11:01:40 -0600

    Cleaned up solution

--- Pot/Pot Kattis.py
+++ Pot/Pot Kattis.py
@@ -1,10 +1,6 @@
 total = 0;
-for i in range(0, int(input())):
-    x = [char for char in input()]    
-    y = ''.join(x[0:-1])
-    power = int(x.pop(-1))
-    total+=int(y)**power
+for i in range(int(input())):
+    x = input()
+    total += int(x[0:len(x)-1])**int(x[len(x)-1])
 
 print(total)
-    
-    
