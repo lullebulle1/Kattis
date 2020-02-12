eac68f6d329337da0915010358ae1697eb1b46e0
ryan mor <56488003+RnMor777@users.noreply.github.com>
2020-02-12 11:05:41 -0600

    Fixed solution

--- Is it Halloween/halloween Kattis.py
+++ Is it Halloween/halloween Kattis.py
@@ -1,5 +1,5 @@
 inputs = input().split(' ')
-if((inputs[0]=="OCT" and inputs[1]=="31") or (inputs[0]=="DEC" and inputs[1]=="24")):
+if((inputs[0]=="OCT" and inputs[1]=="31") or (inputs[0]=="DEC" and inputs[1]=="25")):
     print("yup")
 else:
     print("nope")
