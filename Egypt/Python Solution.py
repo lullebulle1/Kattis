271b53281a45bae1bafa2a71ba225703af9c6d8e
Ryan <ryan.morganti@trojans.dsu.edu>
2020-05-29 20:07:50 -0600

    Added python solution

--- Egypt/Python Solution.py
+++ Egypt/Python Solution.py
@@ -1,8 +1,10 @@
-x = list(map(int, input().split()))
+x = list(map(float, input().split()))
+x.sort()
 
 while (x[0] != 0):
-    if ((x[0]**2 + x[1]**2)**(1/2)):
+    if (x[0]**2 + x[1]**2 - x[2]**2 == 0):
         print ("right")
     else:
         print ("wrong")
-    x = list(map(int, input().split()))
+    x = list(map(float, input().split()))
+    x.sort()
